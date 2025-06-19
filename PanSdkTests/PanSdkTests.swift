4d117595cc9e05b46054b06342b422f9f89adc43
Juan Rodríguez <jrodriguez@mobohubb.com>
2025-06-19 09:24:27 -0500

    Initial Commit

---
+++ PanSdkTests/PanSdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PanSdkTests.swift
+//  PanSdkTests
+//
+//  Created by fpena on 19/06/25.
+//
+
+import Testing
+@testable import PanSdk
+
+struct PanSdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
